7fce9fd64ee5bfab8bda83cf13d33b857643f4e6
Tingxuan Wu <txwu0310@gmail.com>
2023-08-22 22:02:33 -0600

    commit

---
+++ Z-Residual Diagnostics for Detecting Non-Proportional Hazards/qresidual_coxph.R
@@ -0,0 +1,331 @@
+####### coxph package  ####################################################
+qresidual.coxph <- function (fit_coxph, traindata, newdata)
+{
+  if (!requireNamespace("pacman")) {
+    install.packages("pacman")
+  }
+  pacman::p_load(
+    "stringr"
+    # "data.table"
+  )
+  # data_name<- as.character(fit_coxph$call[[3]])
+  # if(!exists(data_name)) stop(paste(data_name, "does not exist"))
+  # traindata<-get(data_name)
+  
+  form<-(fit_coxph$formula)[[3]]
+  group_id_name<-gsub(".*[(]([^.]+)[,].*", "\\1", form)[3]
+  #  group_id_name<-noquote(str_replace_all(string=group_id_name, pattern=" ", repl=""))
+  if(!is.factor(traindata[[group_id_name]])) stop("The group ID must be factor!")
+  if(!is.factor(newdata[[group_id_name]])) stop("The group ID must be factor!")
+  gpnumber<-length(levels(traindata[[group_id_name]]))
+  
+  mf <- model.frame(fit_coxph$formula, traindata)
+  mf_nc<-ncol (mf)
+  #gpnumber<-data.table::uniqueN(mf[,mf_nc])
+  mm <- model.matrix(fit_coxph$formula, traindata)
+  mm_nc <- ncol (mm)
+  if(gpnumber>5){
+    fix_var<-mm[,-c(1,mm_nc),drop=FALSE]
+    explp<-exp(fix_var %*% fit_coxph$coefficients+fit_coxph$frail[mf[,mf_nc]])
+    #z_hat is the estimate of random effect
+    z_hat<-exp(fit_coxph$frail[mf[,mf_nc]])
+  }
+  if(gpnumber<=5){
+    fix_var<-mm[,-c(1,mf_nc:mm_nc),drop=FALSE]
+    coef_number<- ncol(fix_var)
+    frailty<-fit_coxph$coefficients[coef_number+1:gpnumber]
+    explp<-exp(fix_var %*% fit_coxph$coefficients[1:coef_number]+
+                 frailty[mf[,mf_nc]])
+    #z_hat is the estimate of random effect
+    z_hat<-exp(frailty[mf[,mf_nc]])
+  }
+  
+  Y <- mf[[1]]
+  if(!inherits(Y, "Surv")) stop("left hand side not a survival object")
+  if(ncol(Y) != 3) {
+    # making it all in (tstart, tstop) format
+    Y <- Surv(rep(0, nrow(Y)), Y[,1], Y[,2])
+  }
+  
+  # this one gives the baseline cumulative hazard at all the time points;
+  getchz <- function(Y, explp) {
+    death <- (Y[, ncol(Y)] == 1) # this is a TRUE FALSE for the status column
+    dtime <- Y[, ncol(Y) - 1] # this is the tstop
+    
+    time <- sort(unique(dtime)) # unique tstops
+    
+    nevent <- as.vector(rowsum(1 * death, dtime))
+    
+    nrisk <- rev(cumsum(rev(rowsum(explp, dtime)))) # This gives the sum
+    delta <- min(diff(time))/2
+    etime <- c(sort(unique(Y[, 1])), max(Y[, 1]) + delta)  #unique entry times
+    
+    indx <- approx(etime, 1:length(etime), time, method = "constant", 
+                   rule = 2, f = 1)$y
+    
+    esum <- rev(cumsum(rev(rowsum(explp, Y[, 1]))))  #not yet entered
+    nrisk <- nrisk - c(esum, 0)[indx]
+    
+    haz <- nevent/nrisk
+    cumhaz <- cumsum(haz)
+    
+    out<-data.frame(time = time, haz = haz, cumhaz = cumhaz)
+    return (out)
+  }
+  df<-getchz(Y=Y,explp=explp)
+  t<-df$time
+  haz<-df$haz
+  H_0<-df$cumhaz
+  f <- stepfun(t[-1], H_0)
+  f_haz<-stepfun(t[-1], haz)
+  
+  full_data<-rbind(newdata,traindata)
+  mf_new<-model.frame(fit_coxph$formula,full_data)[c(1:nrow(newdata)),]
+  mf_nc_new<- ncol (mf_new)
+  mm_new <-model.matrix(fit_coxph$formula,full_data)[c(1:nrow(newdata)),,drop=FALSE]
+  mm_nc_new <- ncol (mm_new)
+  
+  if(gpnumber>5){
+    fix_var_new<-mm_new[,-c(1,mm_nc_new),drop=FALSE]
+    #z_hat is the estimate of random effect
+    z_hat_new<-exp(fit_coxph$frail[mf_new[,mf_nc_new]])
+    #explp is linear predictor with random effect
+    explp_new<-exp(fix_var_new %*% fit_coxph$coefficients)
+  }
+  if(gpnumber<=5){
+    fix_var_new<-mm_new[,-c(1,mf_nc_new:mm_nc_new),drop=FALSE]
+    coef_number_new<- ncol(fix_var_new)
+    frailty_new<-fit_coxph$coefficients[coef_number_new+1:gpnumber]
+    explp_new<-exp(fix_var_new %*% fit_coxph$coefficients[1:coef_number_new])
+    #z_hat is the estimate of random effect
+    z_hat_new<-as.numeric(exp(frailty_new[mf_new[,mf_nc_new]]))
+  }
+  
+  Y_new <- mf_new[[1]]
+  if(!inherits(Y_new, "Surv")) stop("left hand side not a survival object")
+  if(ncol(Y_new) != 3) {
+    # making it all in (tstart, tstop) format
+    Y_new <- Surv(rep(0, nrow(Y_new)), Y_new[,1], Y_new[,2])
+  }
+  H0_new<-f(Y_new[,2])
+  h0_new<-f_haz(Y_new[,2])
+  #hazard function
+  hazfn<- z_hat_new*as.vector(explp_new)*h0_new
+  #Survival Function
+  SP<- exp(-z_hat_new*as.vector(explp_new)*H0_new)
+  censored <- which(Y_new[,3]==0)
+  n.censored <- length(censored)
+  #NRSP residual
+  RSP <- SP
+  RSP[censored] <- RSP[censored]*runif(n.censored)
+  #R_H(t)
+  log_Rcum_haz<- log(-log(RSP))
+  nrsp <- -qnorm(RSP)
+  attr(nrsp, "SP") <- SP
+  attr(nrsp, "log_Rcum_haz") <- log_Rcum_haz
+  # attr(nrsp, "hazfn") <- hazfn
+  #  attr(nrsp, "z_hat_new") <- z_hat_new
+  return(nrsp)
+}
+
+#basehaz(fit_kidney1,centered = F)
+test.nl.aov1 <- function(qresidual, fitted.values, k.anova=10)
+{
+  lpred.bin <- cut(fitted.values, k.anova)
+  less2_factor<-which(tapply(lpred.bin,lpred.bin,length)<= 2)
+  if(rlang::is_empty(names(less2_factor))){
+    anova(lm(qresidual ~ lpred.bin))$`Pr(>F)`[1]
+  }else{
+    vector_less2_factor<-rep(length(less2_factor))
+    for(j in 1:length(less2_factor)){
+      vector_less2_factor[j]<-which(lpred.bin==names(less2_factor[j]))
+    }
+    new.lpred.bin<- lpred.bin[-vector_less2_factor]
+    new.qresidual<-qresidual[-vector_less2_factor]
+    anova(lm(new.qresidual ~ new.lpred.bin))$`Pr(>F)`[1]
+  }
+}
+
+
+test.nl.aov1.categ <- function(qresidual, fitted.values)
+{
+  lpred.bin <- fitted.values
+  anova(lm(qresidual ~ lpred.bin))$`Pr(>F)`[1]
+}
+
+
+test.var.bartl <- function(qresidual, fitted.values, k=10)
+{
+  lpred.bin <- cut(fitted.values, k)
+  Z_group<- split(qresidual, lpred.bin)
+  check_Z_group<-rep(k)
+  for(i in 1:k)
+  {
+    fun<-function(x) x>2
+    check_Z_group[i]<-fun(length(Z_group[[i]]))
+  }
+  if(all(check_Z_group!=0))
+  {
+    bartlett.test(Z_group)[["p.value"]]
+  }else{
+    Z_group<-Z_group[-which(check_Z_group==0)]
+    bartlett.test(Z_group)[["p.value"]]
+  }
+}
+
+test.var.bartl.categ <- function(qresidual, fitted.values)
+{
+  lpred.bin <- fitted.values
+  Z_group<- split(qresidual, lpred.bin)
+  bartlett.test(Z_group)[["p.value"]]
+}
+
+
+bounds_pvalues <- function (pv)
+{
+  pv <- pv[is.finite(pv)]
+  n <- length (pv)
+  if(n<=0) stop("There is no value in 'pv'")
+  pv.sorted <- sort(pv)
+  corrected.pv <- pmin(1, pv.sorted*n/1:n)
+  pv.min <- min(corrected.pv)
+  pv.min
+}
+
+allresidual.coxph <- function (fit_coxph, traindata, newdata)
+{
+  if (!requireNamespace("pacman")) {
+    install.packages("pacman")
+  }
+  pacman::p_load(
+    "stringr"
+    # "data.table"
+  )
+  # data_name<- as.character(fit_coxph$call[[3]])
+  # if(!exists(data_name)) stop(paste(data_name, "does not exist"))
+  # traindata<-get(data_name)
+  
+  form<-(fit_coxph$formula)[[3]]
+  group_id_name<-gsub(".*[(]([^.]+)[,].*", "\\1", form)[3]
+  #  group_id_name<-noquote(str_replace_all(string=group_id_name, pattern=" ", repl=""))
+  if(!is.factor(traindata[[group_id_name]])) stop("The group ID must be factor!")
+  if(!is.factor(newdata[[group_id_name]])) stop("The group ID must be factor!")
+  gpnumber<-length(levels(traindata[[group_id_name]]))
+  
+  mf <- model.frame(fit_coxph$formula, traindata)
+  mf_nc<-ncol (mf)
+  #gpnumber<-data.table::uniqueN(mf[,mf_nc])
+  mm <- model.matrix(fit_coxph$formula, traindata)
+  mm_nc <- ncol (mm)
+  if(gpnumber>5){
+    fix_var<-mm[,-c(1,mm_nc),drop=FALSE]
+    explp<-exp(fix_var %*% fit_coxph$coefficients+fit_coxph$frail[mf[,mf_nc]])
+    #z_hat is the estimate of random effect
+    z_hat<-exp(fit_coxph$frail[mf[,mf_nc]])
+  }
+  if(gpnumber<=5){
+    fix_var<-mm[,-c(1,mf_nc:mm_nc),drop=FALSE]
+    coef_number<- ncol(fix_var)
+    frailty<-fit_coxph$coefficients[coef_number+1:gpnumber]
+    explp<-exp(fix_var %*% fit_coxph$coefficients[1:coef_number]+
+                 frailty[mf[,mf_nc]])
+    #z_hat is the estimate of random effect
+    z_hat<-exp(frailty[mf[,mf_nc]])
+  }
+  
+  Y <- mf[[1]]
+  if(!inherits(Y, "Surv")) stop("left hand side not a survival object")
+  if(ncol(Y) != 3) {
+    # making it all in (tstart, tstop) format
+    Y <- Surv(rep(0, nrow(Y)), Y[,1], Y[,2])
+  }
+  
+  # this one gives the baseline cumulative hazard at all the time points;
+  getchz <- function(Y, explp) {
+    death <- (Y[, ncol(Y)] == 1) # this is a TRUE FALSE for the status column
+    dtime <- Y[, ncol(Y) - 1] # this is the tstop
+    
+    time <- sort(unique(dtime)) # unique tstops
+    
+    nevent <- as.vector(rowsum(1 * death, dtime))
+    
+    nrisk <- rev(cumsum(rev(rowsum(explp, dtime)))) # This gives the sum
+    delta <- min(diff(time))/2
+    etime <- c(sort(unique(Y[, 1])), max(Y[, 1]) + delta)  #unique entry times
+    
+    indx <- approx(etime, 1:length(etime), time, method = "constant", 
+                   rule = 2, f = 1)$y
+    
+    esum <- rev(cumsum(rev(rowsum(explp, Y[, 1]))))  #not yet entered
+    nrisk <- nrisk - c(esum, 0)[indx]
+    
+    haz <- nevent/nrisk
+    cumhaz <- cumsum(haz)
+    
+    out<-data.frame(time = time, haz = haz, cumhaz = cumhaz)
+    return (out)
+  }
+  df<-getchz(Y=Y,explp=explp)
+  t<-df$time
+  haz<-df$haz
+  H_0<-df$cumhaz
+  f <- stepfun(t[-1], H_0)
+  
+  full_data<-rbind(newdata,traindata)
+  mf_new<-model.frame(fit_coxph$formula,full_data)[c(1:nrow(newdata)),]
+  mf_nc_new<- ncol (mf_new)
+  mm_new <-model.matrix(fit_coxph$formula,full_data)[c(1:nrow(newdata)),,drop=FALSE]
+  mm_nc_new <- ncol (mm_new)
+  
+  if(gpnumber>5){
+    fix_var_new<-mm_new[,-c(1,mm_nc_new),drop=FALSE]
+    #z_hat is the estimate of random effect
+    z_hat_new<-exp(fit_coxph$frail[mf_new[,mf_nc_new]])
+    #explp is linear predictor with random effect
+    explp_new<-exp(fix_var_new %*% fit_coxph$coefficients)
+  }
+  if(gpnumber<=5){
+    fix_var_new<-mm_new[,-c(1,mf_nc_new:mm_nc_new),drop=FALSE]
+    coef_number_new<- ncol(fix_var_new)
+    frailty_new<-fit_coxph$coefficients[coef_number_new+1:gpnumber]
+    explp_new<-exp(fix_var_new %*% fit_coxph$coefficients[1:coef_number_new])
+    #z_hat is the estimate of random effect
+    z_hat_new<-as.numeric(exp(frailty_new[mf_new[,mf_nc_new]]))
+  }
+  
+  Y_new <- mf_new[[1]]
+  if(!inherits(Y_new, "Surv")) stop("left hand side not a survival object")
+  if(ncol(Y_new) != 3) {
+    # making it all in (tstart, tstop) format
+    Y_new <- Surv(rep(0, nrow(Y_new)), Y_new[,1], Y_new[,2])
+  }
+  H0_new<-f(Y_new[,2])
+  #Survival Function
+  SP<- exp(-z_hat_new*as.vector(explp_new)*H0_new)
+  censored <- which(Y_new[,3]==0)
+  n.censored <- length(censored)
+  
+  #NRSP residual
+  RSP <- SP
+  RSP[censored] <- RSP[censored]*runif(n.censored)
+  nrsp <- -qnorm(RSP)
+  #Normalized unmodified SPs (NUSP)
+  USP<-SP
+  USP[USP==1] <- .999999999
+  nusp<- -qnorm(USP)
+  # Unmodified CS residual
+  ucs<- -log(SP)
+  # Modified CS residual
+  MSP<- SP
+  MSP[censored] <- SP[censored]/exp(1)
+  mcs <- -log(MSP)
+  #Normalized MSPs
+  MSP<- SP
+  MSP[censored] <- SP[censored]/exp(1)
+  nmsp<- -qnorm(MSP)
+  #Martingale Residual
+  martg<- Y_new[,3] - ucs
+  #Deviance Residual
+  dev<- sign(martg)* sqrt((-2)*(martg+Y_new[,3]*log(Y_new[,3]-martg)))
+  list(nrsp=nrsp,nusp=nusp,ucs=ucs,mcs=mcs,nmsp=nmsp,martg=martg,dev=dev)
+}
---
+++ Z-Residual Diagnostics for Detecting Non-Proportional Hazards/time_denpendent_piecewise.R
@@ -0,0 +1,97 @@
+setwd("~/cox_time_varying_nonph")
+source("~/qresidual_coxph.R")
+source("~/nonph_data.R")
+library("survival")
+library("EnvStats")
+library("nortest")
+library("backports")
+library("stringr")
+library("timereg")
+library("MASS")
+library("dvmisc")
+
+#########piecewise###############################
+####wrong model#################################################################
+nonph_data<-simul_nonph1(n_clusters=10,n_individuals=50,fv=0.5,
+                    lambda=0.007, alpha=3, 
+                    mean.censor=7.5,beta1=0.3,beta_a=1.35,
+                    beta_b=-1.35,t0=2,corr=0)
+
+table(nonph_data$d)
+fit_coxph_nonph_data <- tryCatch(
+  coxph(Surv(t, d) ~ x1+x2+frailty(grpid,distribution = "gamma"),
+        data = nonph_data),
+  error = function(e) NA,
+  warning = function(w) NA
+)
+coxzph_test_w<-cox.zph(fit_coxph_nonph_data, transform="identity");coxzph_test_w
+
+coxph_qr_w<-qresidual.coxph (fit_coxph = fit_coxph_nonph_data,
+                             traindata = nonph_data,
+                             newdata = nonph_data)
+sw_coxph_w<-shapiro.test(coxph_qr_w)$p.value;sw_coxph_w
+
+## the coefficient of x1 is ph
+anov_x1_w<- test.nl.aov1(qresidual=coxph_qr_w,
+                         fitted.values=nonph_data$x1,
+                         k.anova=10);anov_x1_w
+bl_x1_w<-test.var.bartl(qresidual=coxph_qr_w,
+                        fitted.values=nonph_data$x1,
+                        k=10);bl_x1_w
+
+## the coefficient of x2 is non-ph
+anov_x2_w<- test.nl.aov1(qresidual=coxph_qr_w,
+                      fitted.values=nonph_data$x2,
+                      k.anova=10);anov_x2_w
+bl_x2_w<-test.var.bartl(qresidual=coxph_qr_w,
+                        fitted.values=nonph_data$x2,
+                        k=10);bl_x2_w
+
+anov_lp_w<- test.nl.aov1(qresidual=coxph_qr_w,
+                         fitted.values=fit_coxph_nonph_data$linear.predictors,
+                         k.anova=10);anov_lp_w
+
+bl_lp_w<-test.var.bartl(qresidual=coxph_qr_w,
+                        fitted.values = fit_coxph_nonph_data$linear.predictors,
+                        k=10);bl_lp_w
+
+
+################################################################################
+##########################true model###########################################
+ph_data<-simul_nonph1(n_clusters=10,n_individuals=50,fv=0.5,
+                     lambda=0.007, alpha=3,
+                     mean.censor=7.5,beta1=0.3,beta_a=1.35,
+                     beta_b=1.35,t0=pi/2,corr=0)
+table(ph_data$d)
+fit_coxph_ph_data <- tryCatch(
+  coxph(Surv(t, d) ~ x1+x2 +frailty(grpid,distribution = "gamma"),
+        data = ph_data),
+  error = function(e) NA,
+  warning = function(w) NA
+)
+coxzph_test_t<-cox.zph(fit_coxph_ph_data, transform="identity");coxzph_test_t
+
+coxph_qr_t<-qresidual.coxph (fit_coxph = fit_coxph_ph_data,
+                             traindata = ph_data,
+                             newdata = ph_data)
+sw_coxph_t<-shapiro.test(coxph_qr_t)$p.value;sw_coxph_t
+
+## the coefficient of x1 is ph
+anov_x1_t<- test.nl.aov1(qresidual=coxph_qr_t,
+                         fitted.values=ph_data$x1);anov_x1_t
+bl_x1_t<-test.var.bartl(qresidual=coxph_qr_t,
+                        fitted.values=ph_data$x1);bl_x1_t
+
+## the coefficient of x2 is ph
+anov_x2_t<- test.nl.aov1(qresidual=coxph_qr_t,
+                         fitted.values=ph_data$x2);anov_x2_t
+bl_x2_t<-test.var.bartl(qresidual=coxph_qr_t,
+                        fitted.values=ph_data$x2);bl_x2_t
+
+anov_lp_t<- test.nl.aov1(qresidual=coxph_qr_t,
+                         fitted.values=fit_coxph_ph_data$linear.predictors)
+anov_lp_t
+bl_lp_t<-test.var.bartl(qresidual=coxph_qr_t,
+                        fitted.values = fit_coxph_ph_data$linear.predictors)
+bl_lp_t
+
